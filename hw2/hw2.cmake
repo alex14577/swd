84d57547867648653f981202b1363dad2b5ced3d
root <root@DESKTOP-SRJIQ8J>
2024-08-04 18:47:38 +0300

    Roll back

--- hw2/hw2.cmake
+++
@@ -1,8 +0,0 @@
-add_library(movement_meta  INTERFACE)
-add_library(movement::meta ALIAS movement_meta)
-
-target_include_directories(movement_meta
-    INTERFACE
-        $<BUILD_INTERFACE:${CMAKE_CURRENT_SOURCE_DIR}>
-        $<INSTALL_INTERFACE:${CMAKE_INSTALL_INCLUDEDIR}>
-)
